42fe41bd08c08cc2ed614c9dbf906e5046799a94
Alaront <sasha.prikhodko.02@bk.ru>
2021-04-02 21:20:21 +0300

    add scroll

---
+++ pages/zoo/script/scroll.js
@@ -0,0 +1,14 @@
+const anchors = document.querySelectorAll('a[href*="#"]')
+
+anchors.forEach((anchor) => {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault()
+    
+    const blockID = anchor.getAttribute('href').substr(1)
+    
+    document.getElementById(blockID).scrollIntoView({
+      behavior: 'smooth',
+      block: 'start'
+    })
+  })
+})
