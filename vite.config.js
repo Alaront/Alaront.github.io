9d741f2e8980c0da17c4190a78b31bd3048f3fda
Alexander Prikhodko <alaront.work@gmail.com>
2024-03-11 11:08:20 +0300

    valorant page fix

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,28 @@
 import { defineConfig } from "vite"
+import { resolve } from 'path'
 import vitePugPlugin from 'vite-plugin-pug-transformer';
+import {viteStaticCopy} from "vite-plugin-static-copy";
 
 
 export default defineConfig({
-    plugins: [vitePugPlugin()],
+    plugins: [
+        vitePugPlugin(),
+        viteStaticCopy({
+            targets: [
+                {
+                    src: resolve(__dirname, './pages') + '/[!.]*',
+                    dest: './pages/',
+                }
+            ]
+        })
+    ],
     target: 'es2015',
     assetsInlineLimit: 0,
+    build: {
+        rollupOptions: {
+            input: {
+                main: resolve(__dirname, 'index.html'),
+            },
+        },
+    },
 })
